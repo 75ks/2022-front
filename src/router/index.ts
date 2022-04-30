360be5d9b1b7dd2f8435ea6274c480e03f8a2de0
75ks <kenta.s705@gmail.com>
2022-04-30 15:59:51 +0900

    routerを設定した

---
+++ src/router/index.ts
@@ -0,0 +1,11 @@
+import * as vueRouter from "vue-router";
+
+const routes = [
+];
+
+const router = vueRouter.createRouter({
+  history: vueRouter.createWebHistory(),
+  routes,
+});
+
+export default router;
