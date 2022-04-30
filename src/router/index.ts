4c43627647691910b98432fd56155b6249f112d1
75ks <kenta.s705@gmail.com>
2022-04-30 16:08:02 +0900

    初期表示を設定した

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,11 @@
 import * as vueRouter from "vue-router";
+import Test from '../views/Test.vue';
 
 const routes = [
+  {
+    path: "/",
+    component: Test,
+  },
 ];
 
 const router = vueRouter.createRouter({
