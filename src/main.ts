89a22447501253654034d47b03af109d6d0c41c5
75ks <kenta.s705@gmail.com>
2022-05-07 13:51:09 +0900

    fontawesome(不足分)を追加した

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ import { createPinia } from 'pinia'
 import './index.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(fas)
+library.add(fas, far, fab)
 
-createApp(App).use(router).use(createPinia()).component('fa', FontAwesomeIcon ).mount('#app')
+createApp(App).use(router).use(createPinia()).component('font-awesome-icon', FontAwesomeIcon ).mount('#app')
