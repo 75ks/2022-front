c302c38f507d937dcba8c1c650e97267ec97912a
75ks <kenta.s705@gmail.com>
2022-05-08 00:23:17 +0900

    共通メニューを固定した

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app" class="w-full flex">
-    <div class="w-1/6 bg-black text-white font-bold">menu</div>
+    <div class="w-1/6 h-screen fixed top-0 left-0 right-0 bg-gray-700 text-white font-bold">menu</div>
+    <div class="w-1/5"></div>
     <router-view class="w-full h-screen p-2 bg-gray-200" />
   </div>
 </template>
