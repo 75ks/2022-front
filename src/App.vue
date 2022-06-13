136c0959324d39a56572dbc544a57245dc76c2ce
75ks <kenta.s705@gmail.com>
2022-06-14 00:48:33 +0900

    サイドバーを作成した

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app" class="w-full flex">
-    <!-- TODO: メニューバーを作成する -->
-    <div class="w-1/6 h-screen fixed top-0 left-0 right-0 bg-gray-700 text-white font-bold">menu</div>
-    <div class="w-1/5"></div>
+    <SideBar />
     <router-view class="w-full h-screen p-2 bg-gray-200" />
   </div>
 </template>
+
+<script setup lang="ts">
+import SideBar from './components/Common/SideBar.vue';
+</script>
+
+<style>
+</style>
