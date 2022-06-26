7380fc2a6202d0e195c5440b3f183f16bcfaa85f
75ks <kenta.s705@gmail.com>
2022-06-26 17:14:11 +0900

    メニュー、ランク、予約状態プルダウンに必須項目が追加できるように修正した

--- src/components/SelectOptions/Menu.vue
+++ src/components/SelectOptions/Menu.vue
@@ -1,9 +1,12 @@
 <template>
-  <label for="menu" class="block">メニュー</label>
+  <label for="menu" class="block">
+    メニュー<span v-if="requiredFlg" class="text-red-600">*</span>
+  </label>
   <select
     v-model="selectValue"
     id="menu"
     class="w-48 px-2 py-1 border border-gray-300 active:outline-none focus:outline-none rounded"
+    :class="[requiredFlg ? 'bg-yellow-100' : '']"
   >
     <option v-for="option of selectOptionList" :key="option.code" :value="option.name">
       {{ option.name }}
@@ -16,14 +19,19 @@ import axios from '../../plugins/axios';
 import { SelectOption } from '../../models/selectOption';
 import { computed, ref } from 'vue';
 interface Props {
+  /** 選択値 */
   selectValue: string;
+  /** 必須フラグ */
+  requiredFlg?: boolean;
 }
 
 interface Emits {
   (e: 'update:selectValue', text: string): string;
 }
 
-const props = defineProps<Props>();
+const props = withDefaults(defineProps<Props>(), {
+  requiredFlg: false
+});
 
 const emits = defineEmits<Emits>();
 
---
+++ src/constants/Gender.ts
@@ -0,0 +1,4 @@
+export const Gender = new Map<string, string>([
+  ["1", "男性"],
+  ["2", "女性"]
+]);
