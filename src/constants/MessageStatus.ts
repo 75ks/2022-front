7abfcd89b52de7e65a476ecdbdaf209334acaa23
tkykymok <tkykymok2@icloud.com>
2022-07-09 12:09:15 +0900

    定数の定義を修正

--- src/constants/MessageStatus.ts
+++ src/constants/MessageStatus.ts
@@ -1,6 +1,20 @@
-export const MessageStatus = {
-  Success: 'success',
-  Warning: 'warning',
-  Danger: 'danger',
-  Info: 'info',
+import {ConstantBase} from './ConstantBase';
+
+export class MessageStatusConstant extends ConstantBase{
 }
+
+export const MessageStatus = {
+  SUCCESS: new MessageStatusConstant(1, "success"),
+  WARNING: new MessageStatusConstant(2, "warning"),
+  DANGER: new MessageStatusConstant(3, "danger"),
+  INFO: new MessageStatusConstant(4, "info"),
+};
+
+const getNameByCode = (code: number | null): string => {
+  return <string>codeNameMap.get(code);
+}
+
+const codeNameMap = new Map<number | null, string>();
+Object.values(MessageStatus).forEach((elem) => {
+  codeNameMap.set(elem.code, elem.name);
+});
