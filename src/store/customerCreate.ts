c72ce73b0a17cc921ddf4b0c8c4621aab05aa181
k-takase36 <k.takase36@gmail.com>
2022-07-14 05:54:08 +0900

    フロント側最新化に伴う保存

--- src/store/customerCreate.ts
+++ src/store/customerCreate.ts
@@ -1,31 +1,31 @@
-import { defineStore } from 'pinia';
-import { CustomerCreateData } from '../models/CustomerCreate';
-import axios from '../plugins/axios';
+// import { defineStore } from 'pinia';
+// import { CustomerCreateData } from '../models/CustomerCreate';
+// import axios from '../plugins/axios';
 
-export const useCustomerCreateStore = defineStore({
-  id: "customerCreate",
-  state: () => ({
-    customerCreates: [] as CustomerCreateData[]
-  }),
-  getters: {
-    getCustomerCreates(state): CustomerCreateData[] {
-      return state.customerCreates;
-    },
-  },
-  actions: {
-    async fetchCustomerCreates(): Promise<void> {
-      this.resetCustomerCreates();
-      const { data } = await axios.get("/customerCreates/");
-      this.addCustomerCreates(data);
-    },
-    addCustomerCreates(array: CustomerCreateData[]): void {
-      this.resetCustomerCreates();
-      array.forEach(obj => {
-        this.customerCreates.push(obj);
-      });
-    },
-    resetCustomerCreates(): void {
-      this.customerCreates.splice(0);
-    }
-  },
-})
+// export const useCustomerCreateStore = defineStore({
+//   id: "customerCreate",
+//   state: () => ({
+//     customerCreates: [] as CustomerCreateData[]
+//   }),
+//   getters: {
+//     getCustomerCreates(state): CustomerCreateData[] {
+//       return state.customerCreates;
+//     },
+//   },
+//   actions: {
+//     async fetchCustomerCreates(): Promise<void> {
+//       this.resetCustomerCreates();
+//       const { data } = await axios.get("/customerCreates/");
+//       this.addCustomerCreates(data);
+//     },
+//     addCustomerCreates(array: CustomerCreateData[]): void {
+//       this.resetCustomerCreates();
+//       array.forEach(obj => {
+//         this.customerCreates.push(obj);
+//       });
+//     },
+//     resetCustomerCreates(): void {
+//       this.customerCreates.splice(0);
+//     }
+//   },
+// })
