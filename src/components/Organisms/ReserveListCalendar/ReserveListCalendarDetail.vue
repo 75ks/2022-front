08e4410c178d69f998370f87fdff5efeab81ee86
75ks <kenta.s705@gmail.com>
2022-08-16 01:10:47 +0900

    予約カレンダーリファクタリング

--- src/components/Organisms/ReserveListCalendar/ReserveListCalendarDetail.vue
+++ src/components/Organisms/ReserveListCalendar/ReserveListCalendarDetail.vue
@@ -2,12 +2,27 @@
   <div class="custom-height p-2 mt-2 bg-white">
     <select
       v-model="calendarSelectValue"
-      class="px-2 py-2 text-sm text-gray-700 border border-gray-300 rounded-md focus:outline outline-blue-300"
+      class="h-10 p-2 text-sm text-gray-700 border border-gray-300 rounded-md focus:outline outline-blue-300"
     >
       <option value="1">日</option>
       <option value="2">週</option>
       <option value="3">月</option>
     </select>
+    <div class="w-full h-12 py-2 flex justify-between font-bold text-2xl bg-white sticky top-0 z-50">
+      <button
+        @click="prevCalendar(currentDate)"
+        class="text-black hover:text-gray-500"
+      >
+        ◀︎
+      </button>
+      <p>{{ currentDateFormat }}</p>
+      <button
+        @click="nextCalendar(currentDate)"
+        class="text-black hover:text-gray-500"
+      >
+        ▶︎
+      </button>
+    </div>
     <div v-if="calendarSelectValue === '1'">
       <ReserveListCalendarDay
         :reserve-list="reserveList"
@@ -28,7 +43,7 @@
         @next-week="nextWeek"
       />
     </div>
-    <div class="custom-height" v-if="calendarSelectValue === '3'">
+    <div class="custom-height-2" v-if="calendarSelectValue === '3'">
       <ReserveListCalendarMonth
         :reserve-list="reserveList"
         :current-date="currentDate"
@@ -70,6 +85,36 @@ const currentDateFormat = computed<string>(() => {
 /** 曜日 */
 const dayOfWeek: string[] = ["日", "月", "火", "水", "木", "金", "土"];
 
+/** ◀ボタンクリックイベント */
+const prevCalendar = (value: moment.Moment): void => {
+  switch (calendarSelectValue.value) {
+    case "1":
+      prevDay(value);
+      break;
+    case "2":
+      prevWeek(value);
+      break;
+    case "3":
+      prevMonth(value);
+      break;
+  }
+}
+
+/** ▶︎ボタンクリックイベント */
+const nextCalendar = (value: moment.Moment): void => {
+  switch (calendarSelectValue.value) {
+    case "1":
+      nextDay(value);
+      break;
+    case "2":
+      nextWeek(value);
+      break;
+    case "3":
+      nextMonth(value);
+      break;
+  }
+}
+
 /** -1(日) */
 const prevDay = (value: moment.Moment): void => {
   currentDate.value = moment(value).subtract(1, "day");
@@ -104,6 +149,10 @@ const nextMonth = (value: moment.Moment): void => {
 <style>
   .custom-height {
     /** カレンダー高さ微調整(ヘッダー:48px - border:1px) */
-    height: calc(100% - 47px);
+    height: calc(100% - 48px - 1px);
+  }
+  .custom-height-2 {
+    /** カレンダー高さ微調整(カレンダー表示形式プルダウン:38px - 現在日時:48px - border:1px) */
+    height: calc(100% - 38px - 48px - 1px);
   }
 </style>
