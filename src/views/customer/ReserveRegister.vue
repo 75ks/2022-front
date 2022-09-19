06a00579009419f77e848b07cdebd9ff0c781a55
75ks <kenta.s705@gmail.com>
2022-09-19 18:05:14 +0900

    顧客用予約画面を作成した

--- src/views/customer/ReserveRegister.vue
+++ src/views/customer/ReserveRegister.vue
@@ -1,115 +1,70 @@
 <template>
-  <div class="w-full h-full">
-    <p class="mt-5 mb-10 text-center font-bold text-2xl">予約</p>
-    <p class="mb-10 m-auto text-center text-sm">◆予約情報を入力して「登録」ボタンをクリックしてください。</p>
-    <div
-      v-if="message.messageList.length"
-      class="pb-10 w-1/3 m-auto"
-      :class="message.messageType === MessageStatus.DANGER.code ? 'text-red-500' : 'text-green-500'"
-    >
-      <ul v-for="(mes, index) in message.messageList" :key="index">
-        <li>※{{ mes }}</li>
-      </ul>
-    </div>
-    <div class="grid gap-6 mb-6 grid-cols-2">
-      <InputWithLabel
-        v-model:input-value="state.screenObj.lastName"
-        label="姓"
-        :requiredFlg="true"
-      />
-      <InputWithLabel
-        v-model:input-value="state.screenObj.firstName"
-        label="名"
-        :requiredFlg="true"
-      />
-      <InputWithLabel
-        v-model:input-value="state.screenObj.lastNameKana"
-        label="セイ"
-        :requiredFlg="true"
-      />
-      <InputWithLabel
-        v-model:input-value="state.screenObj.firstNameKana"
-        label="メイ"
-        :requiredFlg="true"
-      />
-      <DatePickerWithLabel
-        v-model:inputValue="state.screenObj.birthday"
-        label="生年月日"
-        :requiredFlg="false"
-      />
-      <InputWithLabel
-        v-model:input-value="state.screenObj.age"
-        label="年齢"
-        :requiredFlg="false"
-      />
-      <SelectBoxWithLabel
-        v-model:select-value="state.screenObj.gender"
-        :options="GenderList"
-        label="性別"
-        :requiredFlg="true"
-      />
-      <InputWithLabel
-        v-model:input-value="state.screenObj.postalCode"
-        label="郵便番号"
-        :requiredFlg="false"
-      />
-      <SelectBoxWithLabel
-        v-model:select-value="state.screenObj.prefectureId"
-        :options="PrefectureIdList"
-        label="都道府県"
-        :requiredFlg="false"
-      />
-      <InputWithLabel
-        v-model:input-value="state.screenObj.address1"
-        label="市区町村"
-        :requiredFlg="false"
-      />
-      <InputWithLabel
-        v-model:input-value="state.screenObj.address2"
-        label="市区町村以下"
-        :requiredFlg="false"
-      />
-      <InputWithLabel
-        v-model:input-value="state.screenObj.address3"
-        label="建物、部屋番号"
-        :requiredFlg="false"
-      />
-      <InputWithLabel
-        v-model:input-value="state.screenObj.phoneNumber"
-        label="電話番号"
-        :requiredFlg="false"
-      />
-      <InputWithLabel
-        v-model:input-value="state.screenObj.email"
-        label="メールアドレス"
-        type="email"
-        :requiredFlg="true"
-      />
-    </div>
-    <div class="w-1/5 m-auto mb-5">
-      <CustomButton
-        class="w-full mt-10"
-        :button-name="'登録'"
-        :button-color-number="1"
-        @click="register"
-      />
+  <div class="w-full">
+    <Loading :is-loading="isLoading"/>
+    <div class="w-2/3 m-auto p-8 mt-20 bg-white">
+      <p class="pb-10 text-center font-bold text-2xl">予約</p>
+      <p class="pb-10 m-auto text-center text-sm">◆予約情報を入力して「登録」ボタンをクリックしてください。</p>
+      <div
+        v-if="message.messageList.length"
+        class="pb-10 w-1/3 m-auto"
+        :class="message.messageType === MessageStatus.DANGER.code ? 'text-red-500' : 'text-green-500'"
+      >
+        <ul v-for="(mes, index) in message.messageList" :key="index">
+          <li>※{{ mes }}</li>
+        </ul>
+      </div>
+      <div class="flex flex-col justify-center items-center">
+        <div class="w-2/3 pb-5">
+          <SelectBoxWithLabel
+            v-model:select-value="state.screenObj.stuffId"
+            targetUrl="/selectOption/stuffs"
+            :required-flg="true"
+            label="スタッフ"
+          />
+        </div>
+        <div class="w-2/3 pb-5">
+          <SelectBoxWithLabel
+            v-model:select-value="state.screenObj.menuId"
+            targetUrl="/selectOption/menus"
+            :required-flg="true"
+            label="メニュー"
+          />
+        </div>
+        <div class="w-2/3 pb-10">
+          <DateTimePickerWithLabel
+            v-model:inputValue='state.screenObj.reserveDateTime'
+            :required-flg="true"
+            label="予約日時"
+          />
+        </div>
+        <div class="w-2/3">
+          <CustomButton
+            class="w-full"
+            button-name="登録"
+            :button-color-number="1"
+            @click="register"
+          />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, reactive } from 'vue';
+import { computed, reactive, ref } from 'vue';
+import { useRouter } from 'vue-router';
 import { useMessageStore } from '../../store/message';
+import { useCustomerStore } from '../../store/customer';
 import { MessageStatus } from '../../constants/MessageStatus';
 import { ReserveRegisterScreenObj } from '../../models/screenObj/ReserveRegisterScreenObj';
-import { GenderList } from '../../constants/Gender';
-import { PrefectureIdList } from '../../constants/PrefectureId';
-import InputWithLabel from '../../components/Molecules/InputWithLabel.vue';
-import DatePickerWithLabel from '../../components/Molecules/DatePickerWithLabel.vue';
 import CustomButton from '../../components/Atoms/Button/CustomButton.vue';
 import SelectBoxWithLabel from '../../components/Molecules/SelectBoxWithLabel.vue';
+import DateTimePickerWithLabel from '../../components/Molecules/DateTimePickerWithLabel.vue';
+import Loading from '../../components/Atoms/Layout/Loading.vue';
 
 const messageStore = useMessageStore();
+const customerStore = useCustomerStore();
+const router = useRouter();
 
 const message = computed(() => {
   return messageStore.getMessage;
@@ -123,8 +78,18 @@ const state = reactive<State>({
   screenObj: new ReserveRegisterScreenObj(),
 });
 
+/** ローティングフラグ */
+const isLoading = ref<boolean>(false);
+
 /** 登録ボタンクリックイベント */
-const register = async () => {
+const register = async (): Promise<void> => {
+  try {
+    isLoading.value = !isLoading.value;
+    await customerStore.reserveRegister(state.screenObj);
+    router.push("/customer/profile");
+  } catch (error) {
+    isLoading.value = !isLoading.value;
+  }
 };
 </script>
 
