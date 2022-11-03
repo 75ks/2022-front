6fd30ccfdacf4fbf74ab2d887a7fa8deeda39367
75ks <kenta.s705@gmail.com>
2022-11-03 11:26:58 +0900

    顧客ホーム初期表示APIのレスポンス構造をServer側に併せて修正した

---
+++ src/models/CustomerHomeReserve.ts
@@ -0,0 +1,12 @@
+export class CustomerHomeReserve {
+  /** メニュー */
+  menu: string = "";
+  /** 料金 */
+  price: number | null = null;
+  /** 予約日時 */
+  reserveDatetime: string = "";
+  /** スタッフ(姓) */
+  lastName: string = "";
+  /** スタッフ(名) */
+  firstName: string = "";
+}
---
+++ src/models/CustomerHomeSales.ts
@@ -0,0 +1,12 @@
+export class CustomerHomeSales {
+  /** メニュー */
+  menu: string = "";
+  /** 料金 */
+  price: number | null = null;
+  /** 来店日時 */
+  salesDatetime: string = "";
+  /** スタッフ(姓) */
+  lastName: string = "";
+  /** スタッフ(名) */
+  firstName: string = "";
+}
